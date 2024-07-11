f37991cc9d4aee06a19d8f6ed24e849a155ad545
Igor Gabriel <rogiborgessouza@outlook.com>
2024-07-11 16:02:12 -0300

    chore: improve type InputProps

--- src/components/input.tsx
+++ src/components/input.tsx
@@ -1,24 +1,40 @@
 import { ReactNode } from "react";
-import { Platform, TextInput, TextInputProps, View } from "react-native";
+import {
+  Platform,
+  TextInput,
+  TextInputProps,
+  View,
+  ViewProps,
+} from "react-native";
 import { clsx } from "clsx";
 
 import { colors } from "@/styles/colors";
 
 type Variants = "primary" | "secondary" | "tertiary";
 
-type InputProps = {
+type InputProps = ViewProps & {
   children: ReactNode;
   variant?: Variants;
 };
 
-function Input({ children, variant = "primary" }: InputProps) {
+function Input({
+  children,
+  variant = "primary",
+  className,
+  ...rest
+}: InputProps) {
   return (
     <View
-      className={clsx("w-full h-16 flex-row gap-2 items-center", {
-        "h-14 px-4 rounded-lg border border-zinc-800": variant != "primary",
-        "bg-zinc-950": variant === "secondary",
-        "bg-zinc-900": variant === "tertiary",
-      })}
+      className={clsx(
+        "w-full h-16 flex-row gap-2 items-center",
+        {
+          "h-14 px-4 rounded-lg border border-zinc-800": variant != "primary",
+          "bg-zinc-950": variant === "secondary",
+          "bg-zinc-900": variant === "tertiary",
+        },
+        className
+      )}
+      {...rest}
     >
       {children}
     </View>
